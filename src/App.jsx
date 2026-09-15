343656e9e98097b2915036055eccd581a204fef1
Estela Mariano <estela.mariano@icloud.com>
2026-09-15 10:58:34 -0300

    feat: Instalando dependências de rotas e importando no App'

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/NavBar'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Produto from './pages/Produtos'
+import Contato from './pages/Contato'
+
 const App = () => {
   return (
     <>
